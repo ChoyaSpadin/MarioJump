be3f733cd0382e872a74a41a14e56d2752a16811
ChoyaSpadin <choya.spadin@gmail.com>
2022-07-22 22:08:26 -0300

    (WIP)timer

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
 const startBtn = document.querySelector(".startBtn");
 const restartBtn = document.querySelector(".restartBtn");
+const score = document.getElementById("score");
 
 restartBtn.classList.add("hide");
+score.classList.add("hide");
 
 const jump = () => {
   mario.classList.add("jump");
@@ -13,9 +15,10 @@ const jump = () => {
 };
 const start = () => {
   pipe.classList.add("start");
-
   startBtn.classList.add("hide");
 };
+
+document.addEventListener("keydown", jump);
 
 const loop = setInterval(() => {
   const pipePosition = pipe.offsetLeft;
@@ -33,11 +36,83 @@ const loop = setInterval(() => {
     mario.src = "./images/game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "50px";
-
-    restartBtn.classList.add("display");
-
+    restartBtn.classList.remove("hide");
+    score.classList.remove("hide");
+    pause();
+    saveScore();
     clearInterval(loop);
   }
 }, 10);
 
-document.addEventListener("keydown", jump);
+let hour = 0;
+let minute = 0;
+let second = 0;
+let millisecond = 0;
+
+let cron;
+
+function startTimer() {
+  pause();
+  cron = setInterval(() => {
+    timer();
+  }, 10);
+}
+
+function pause() {
+  clearInterval(cron);
+}
+
+function reset() {
+  hour = 0;
+  minute = 0;
+  second = 0;
+  millisecond = 0;
+  // document.getElementById("hour").innerText = "00";
+  document.getElementById("minute").innerText = "00";
+  document.getElementById("second").innerText = "00";
+  document.getElementById("millisecond").innerText = "000";
+}
+
+document.form_main.start.onclick = () => startTimer();
+document.form_main.pause.onclick = () => pause();
+document.form_main.reset.onclick = () => reset();
+
+function timer() {
+  if ((millisecond += 10) === 1000) {
+    millisecond = 0;
+    second++;
+  }
+  if (second === 60) {
+    second = 0;
+    minute++;
+  }
+  if (minute === 60) {
+    minute = 0;
+    hour++;
+  }
+  // document.getElementById("hour").innerText = returnData(hour);
+  document.getElementById("minute").innerText = returnData(minute);
+  document.getElementById("second").innerText = returnData(second);
+  document.getElementById("millisecond").innerText = returnData(millisecond);
+}
+
+function returnData(input) {
+  return input > 10 ? input : `0${input}`;
+}
+
+function saveScore() {
+  const minute = document.getElementById("minute").innerText;
+  const second = document.getElementById("second").innerText;
+  const millisecond = document.getElementById("millisecond").innerText;
+  const scoreMillisecond = (document.getElementById(
+    "scoreMillisecond"
+  ).innerHTML = millisecond);
+  const scoreSecond = (document.getElementById("scoreSecond").innerHTML =
+    second);
+  const scoreMinute = (document.getElementById("scoreMinute").innerHTML =
+    minute);
+  // const xhr = new XMLHttpRequest();
+  // xhr.open("POST", "http://localhost:3000/save", true);
+  // xhr.setRequestHeader("Content-Type", "application/json");
+  // xhr.send(JSON.stringify(data));
+}
